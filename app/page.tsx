7b9a2b281d7fc5d8affd9576479e068445cfcb56
pickingname <trintrinir@icloud.com>
2024-10-20 10:24:07 +0700

    feat: add notes

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,13 @@ export default function Home() {
       <p className="text-2xl pb-5">Get the current weather for your location</p>
       <GetLocation />
       <FetchAndDisplayData />
+      <div>
+        {/* NOTE: loads this note and warning with the font so the font is loaded and cached first before the weather data is fetched */}
+        <p className="text-center text-neutral-600 pt-8 font-outfit">
+          Note: Current weather conditions are updated every 15 minutes based on
+          our weather model data
+        </p>
+      </div>
     </div>
   );
 }
