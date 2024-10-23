d3c7db8b5e3327f126c73aca0d98f643a4076b3a
pickingname <trintrinir@icloud.com>
2024-10-23 22:18:21 +0700

    feat add incompat banner

---
+++ components/parts/incompatBanner.tsx
@@ -0,0 +1,35 @@
+"use client";
+import React, { useEffect, useState } from "react";
+
+const BrowserWarningBanner = () => {
+  const [isChromeBased, setIsChromeBased] = useState(false);
+
+  useEffect(() => {
+    const userAgent = navigator.userAgent;
+
+    const isChrome =
+      /Chrome|Chromium/.test(userAgent) && !/Edg/.test(userAgent);
+    const isEdge = /Edg/.test(userAgent);
+
+    if (isChrome || isEdge) {
+      setIsChromeBased(true);
+    }
+  }, []);
+
+  if (!isChromeBased) {
+    return null;
+  }
+
+  return (
+    <div className="bg-neutral-800 text-white font-outfit text-center p-1.5 top-0 left-0 right-0 z-50">
+      <p className="m-0 text-lg">
+        Did you know? This website's user interface isn't rendering correctly in
+        Chrome-based browsers.{" "}
+        <span className="text-red-500">It seems you're using one.</span> Please
+        consider switching to Firefox or Safari.
+      </p>
+    </div>
+  );
+};
+
+export default BrowserWarningBanner;
